abbffdf87655a869d37e7d8acc97cfc3e4f768ce
Brayan Arrieta Alfaro <brayanarrietaalfaro@gmail.com>
2017-04-30 10:52:23 -0600

    amperson

--- main/app.js
+++ main/app.js
@@ -16,11 +16,12 @@ angular.module('mainModule',["ngRoute"])
             templateUrl:'Avistamientos/avistamientos.html',
             controller: 'avistamientosCtrl'
         })
-        .when("/areas",{
-            templateUrl:'Maps/areas.html',
-            controller: 'areasCtrl'
+        .when("/avistamiento/crear",{
+            templateUrl:'Avistamientos/create.html',
+            controller: 'crearController'
         })
 
-        
+
+
     }
 ]);
---
+++ main/controllers/crearController.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Arrieta on 30/4/2017.
+ */
+angular.module('mainModule')
+    .controller('crearController', function($scope) {});
